ec270f128c188e3b5cb07d38ffac1364c445402c
Kim-Hyung-Min9 <khm9403@gmail.com>
2026-01-06 16:13:45 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -90,3 +90,41 @@ form.addEventListener("submit", (e) => {
 
 // ===== Footer Year =====
 $("#year").textContent = new Date().getFullYear();
+
+// email 전송 가능한 객체 가져오기
+
+(function(){
+    // 모든 html요소가 로드 되면 실행되는 함수
+
+    emailjs.init("OSCZBjeyqmo7QB2NQ");
+})();
+
+
+
+
+
+
+function sendMail() {
+    let from_name = document.querySelector("input[name='from_name']").value;
+    let name = "김형민";
+    let to_name = "김형민";
+    let email = "khm9403@gmail.com";
+    let message = document.querySelector("textarea[name='message']").value;
+
+    // parameter 방식으로 변환
+    let params = {
+        from_name : from_name,
+        name : name,
+        to_name : to_name,
+        email : email,
+        message : message
+    }
+
+    // console.log(params);
+    
+
+    emailjs.send("service_i4wvkfm", "template_ae7zzpo", params).then(function(res){alert('이메일 발송이 완료되었습니다.')});
+
+
+}
+
